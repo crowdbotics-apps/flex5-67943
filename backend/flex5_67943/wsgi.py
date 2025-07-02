baffb993e344daf60f43e90140634ec7c1cf960f
Crowdbotics <team@crowdbotics.com>
2025-07-02 07:04:27 +0000

    Initial commit

---
+++ backend/flex5_67943/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for flex5_67943 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "flex5_67943.settings"
+)
+
+application = get_wsgi_application()
